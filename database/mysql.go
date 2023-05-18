02be15102e800fe9ebe38e5258a42b3869f78512
Gede Indra Adi Brata <indrabrata599@gmail.com>
2023-05-18 09:10:23 +0700

    Create : mysql connection

--- database/mysql.go
+++ database/mysql.go
@@ -1,1 +1,46 @@
 package database
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+type DB struct {
+	*gorm.DB
+}
+
+func MySQLConnect() (*DB, error) {
+	db, err := MySQLInit()
+
+	if err != nil {
+		log.Fatal("Error connecting to database: ", err.Error())
+	}
+
+	return &DB{db}, nil
+}
+
+func MySQLInit() (*gorm.DB, error) {
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASS"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	)
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Silent),
+	})
+
+	if err != nil {
+		log.Fatal("Error connecting to database: ", err.Error())
+	}
+
+	return db, err
+}
